194a95371ec128c09e50ec7d07cbafe60a949b73
chenzelin <284394371@qq.com>
2014-09-17 18:40:11 +0800

    add new

---
+++ helloWorldNew/src/com/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+﻿package com.helloworld;
+
+public class HelloWorld {
+	/******
+	 * 你好OK
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("你好");
+	}
+}
